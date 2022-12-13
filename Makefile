36729371d582f7b7501252d1b2c478d3e3dc0397
|°_°| <grivel.theo@protonmail.com>
2022-12-13 23:23:28 +0100

    main.c: create a start of parsing
    * Makefile add db rule
    test:
    $ ./microshell
    $ ./microshell ls
    $ ./microshell ls -la
    $ ./microshell ls -la ";" cat -n

--- Makefile
+++ Makefile
@@ -3,11 +3,15 @@ NAME	=	microshell
 CFLAGS	+=	-Wall
 CFLAGS	+=	-Wextra
 CFLAGS	+=	-Werror
+CFLAGS	+=	-g
 
 SRCS	=	srcs/main.c
 
 all:
 	gcc $(CFLAGS) $(SRCS) -o $(NAME)
+
+db: all
+	lldb ./$(NAME)
 
 site/index.html: diagram
 	mkdir -p site/assets
--- srcs/main.c
+++ srcs/main.c
@@ -1,6 +1,65 @@
 #include	<stdio.h>
+#include	<string.h>
 
-int	main(void)
+typedef struct s_command {
+	char	*bin;
+	char	**args;
+}	t_command;
+
+static void	print_command(t_command *cmd)
+{
+	int	index;
+
+	printf("Bin: %s\n", cmd->bin);
+	index = 0;
+	while (cmd->args[index])
+	{
+		printf("args[%d]: %s\n", index, cmd->args[index]);
+		++index;
+	}
+}
+
+static void	find_command(char ***arguments, t_command *cmd)
+{
+	int	index;
+
+	cmd->bin = **arguments;
+	cmd->args = *arguments;
+	index = 0;
+	while ((*arguments)[index] && strcmp((*arguments)[index], ";"))
+		++index;
+	if ((*arguments)[index] == 0)
+	{
+		*arguments = &((*arguments)[index]);
+	}
+	else
+	{
+		(*arguments)[index] = 0;
+		*arguments = &((*arguments)[index + 1]);
+	}
+}
+
+static int	microshell(char **arguments)
+{
+	t_command	cmd;
+
+	while (*arguments)
+	{
+		find_command(&arguments, &cmd);
+		print_command(&cmd);
+	}
+
+	return 0;
+}
+
+int	main(int argc, char **argv)
 {
 	printf("Microshell\n");
+
+	if (argc == 1)
+		return 0;
+
+	++ argv;
+
+	return microshell(argv);
 }
